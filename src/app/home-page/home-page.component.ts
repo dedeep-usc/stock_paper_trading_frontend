f59f61c7480bea5026e7218b081d8d64933a9bb2
Dedeep Reddy S <dedeep@Dedeeps-MBP.lan>
2022-03-27 18:22:02 -0700

    state services

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -37,13 +37,18 @@ export class HomePageComponent implements OnInit {
     var stock_results_tab = this.stock_results_tab;
     console.log("In fill_data");
     if (ticker == "home" || ticker == "HOME") {
+      console.log("Resetting state services.");
+      stock_results_tab.reset_state_services();
       stock_results_tab.home_ticker = true;
+      stock_results_tab.alerts = [];
+      stock_results_tab.ticker = "home";
       return;
     }
     
     stock_results_tab.home_ticker = false;
-    stock_results_tab.get_stock_data(ticker);
     stock_results_tab.ticker = ticker.toUpperCase();
+    // stock_results_tab.get_stock_data(ticker);
+    stock_results_tab.get_stock_data_new(ticker);
     // this.search_box.isLoading = false;
     this.search_box.filtered_tickers = [];
   }
---
+++ src/app/services/company_price/company-price.service.ts
@@ -0,0 +1,122 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CrudService } from 'src/app/shared/crud.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyPriceService {
+
+  constructor(
+    private crud: CrudService
+  ) { }
+
+  data = {
+    "company_name": "",
+    "data": true,
+    "c": 0,
+    "d": 0,
+    "dp": 0,
+    "h": 0,
+    "l": 0,
+    "o": 0,
+    "pc": 0,
+    "t": 0 
+  }
+
+  error_data = {
+    "error": true,
+    "reason": "",
+    "company_name": "",
+    "data": true
+  }
+
+  private _company_price_result = new BehaviorSubject(this.data);
+  readonly company_price$ = this._company_price_result.asObservable();
+
+  ticker = "";
+
+  get_values() {
+    return this._company_price_result.getValue();
+  }
+
+  reset_state() {
+    this._company_price_result.next(this.data);
+  }
+
+  private set_values(val) {
+    this._company_price_result.next(val);
+  }
+
+  handle_error(data) {
+    this.error_data["reason"] = data.message;
+    this.error_data["company_name"] = data.company_name;
+    this.set_values(this.error_data);
+  }
+
+  get_company_price(ticker) {
+
+    if (this.check_valid_data_present(ticker)) {
+      console.log(`CompanyPriceService already has data for the ticker: ${ticker}. Will return.`);
+      return;
+    }
+
+    this.ticker = ticker.toUpperCase();
+
+    console.log(`CompanyPriceService doesn't have data for the ticker: ${ticker}. Will fetch from backend and return.`);
+
+    this.crud.get_company_latest_price(this.ticker).subscribe(data => {
+
+      console.log(`Current ticker: ${this.ticker}`);
+      console.log(`Company from the API result is: ${data.company_name}`);
+      if (data.company_name != this.ticker) {
+        console.log("Current company is different from the company from the data.")
+      }
+
+      if ("error" in data) {
+        this.handle_error(data);
+        return;
+      }
+
+      console.log("The result from get_company_latest_price: " + JSON.stringify(data));
+
+      console.log("Current ticker: " + this.ticker);
+      console.log("The company from the result is: " + data.company_name);
+
+      this.update_values_from_data(data);
+
+    })
+
+  }
+
+  update_values_from_data(data) {
+    var company_name = data.company_name;
+    data = data.message;
+    var temp_data = {
+      "company_name": company_name,
+      "data": true,
+      "c": data.c,
+      "d": data.d,
+      "dp": data.dp,
+      "h": data.h,
+      "l": data.l,
+      "o": data.o,
+      "pc": data.pc,
+      "t": data.t
+    }
+
+    this.set_values(temp_data);
+  }
+
+  check_valid_data_present(ticker) {
+    var cur_data = this._company_price_result["_value"];
+
+    if (cur_data["data"] && !("error" in cur_data) && cur_data["company_name"] == ticker.toUpperCase()) {
+      return true;
+    }
+
+    return false;
+
+  }
+
+}
